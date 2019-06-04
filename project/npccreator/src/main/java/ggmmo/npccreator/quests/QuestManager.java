7994d5018783c2a370b94ea03d589570d1af2e2c
Lachlan <mindcraftlachlan@outlook.com>
2019-06-04 23:14:14 +1000

    Quest Manager
    Setitng up the base file for the quest manager

---
+++ project/npccreator/src/main/java/ggmmo/npccreator/quests/QuestManager.java
@@ -0,0 +1,15 @@
+package ggmmo.npccreator.quests;
+
+import ggmmo.npccreator.NPCQuests;
+
+public class QuestManager {
+    private NPCQuests plugin = NPCQuests.getPlugin();
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
